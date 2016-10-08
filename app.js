1ed8ace606283802a75f6c97c2f4286e4ae29a30
Бараблин Михаил Васильевич <mbarablin@at-consulting.ru>
2016-10-08 17:24:57 +0300

    feature: zeromq logging via push

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var process = require('process');
+
+var zmq = require('zmq')
 
 var index_router = require('./routes/index');
 var users_router = require('./routes/users');
 
 var app = express();
 index_router.app = app; // making a link to application from within
+users_router.app = app; // making a link to application from within
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +25,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// middleware function to send log message via zeromq
+app.use(function(req, res, next){
+    console.log('middleware!!!');
+    app.locals.zmq_log(req.method + ' ' + req.url);
+    next();
+});
 
 app.use('/', index_router);
 app.use('/users', users_router);
@@ -60,5 +71,24 @@ app.use(function(err, req, res, next) {
 // locals
 
 app.locals.title = "NodeJS REST example";
+app.locals.pid = process.pid;
+
+// zeromq
+
+app.locals.zmq_sock_name = 'ipc:///tmp/sock';
+app.locals.zmq_sock = zmq.socket('push');
+app.locals.zmq_sock.connect(app.locals.zmq_sock_name);
+app.locals.zmq_sock.send(app.locals.title + ' started');
+
+// function to log with zeromq socket.
+app.locals.zmq_log = function(str){
+    app.locals.zmq_sock.send(app.locals.title +
+                             ' PID:' + process.pid +
+                             ', ' +
+                             str
+                            );
+};
+
+app.locals.zmq_log('started');
 
 module.exports = app;
